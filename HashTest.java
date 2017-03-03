491cdcf51d1667580586191422ed3bc7307fcf47
jaymspeights <jaymspeights@gmail.com>
2017-03-02 19:54:50 -0600

    First Push

---
+++ HashTest.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+public class HashTest {
+
+  public static void main(String[] args) {
+      ArrayList<String> names = new ArrayList();
+      boolean stop = false;
+      String n;
+      int count = 0;
+      ArrayList<Maze> mazes = new ArrayList();
+      System.out.println("Searching...");
+      while(!stop){
+        Maze m = new Maze(10, 10);
+        mazes.add(m);
+        while(!m.generate());
+        n = m.hash();
+        for (int i = 0; i < names.size(); i++){
+          if (names.get(i).equals(n)){
+            if (!m.toString().equals(mazes.get(i).toString())){
+              m.draw("Maze1");
+              mazes.get(i).draw("maze2");
+              stop = true;
+              System.out.println("Hash collision found at " + count);
+            }
+            System.out.println("Duplicate mazes found at " + count);
+          }
+        }
+        count++;
+        names.add(n);
+      }
+
+
+  }
+
+}
